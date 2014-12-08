44ab5a7d9bd21959fb0934b6c1be6bae8bd5ad32
Stuart Varrall <github@fpstudios.com>
2014-12-08 22:45:08 +0000

    Added Stuart Varrall

--- js/main.js
+++ js/main.js
@@ -193,9 +193,9 @@ var arrData =
         'name': 'Jon Park'
     }, {
         'companies': [
-            
+            'BubblePix',
             'Fluid Pixel',
-            'BubblePix'
+            'Reflections Interactive'
         ],
         'name': 'Stuart Varrall'
     },];
